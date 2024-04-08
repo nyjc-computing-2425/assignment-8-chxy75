114482b26b5b8430dc476f1c0232a1abb6c634ab
chxy75 <160509015+chxy75@users.noreply.github.com>
2024-04-08 00:25:46 +0000

    final

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 # Built-in imports
+def reverse(text):
+  if len(text) <= 1:
+      return text
+  else:
+      return reverse(text[1:]) + text[0]
 
+def is_palindrome(text):
+  text =  text.replace('!','').replace('?','').replace(' ','')
+  if text[0] == text[-1]:
+    while len(text) >= 2:
+     is_palindrome(text[1:-1])
+    else :
+      return True
+  else:
+    return False
