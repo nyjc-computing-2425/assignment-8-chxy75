4ca79a80a6b7e4c9a7e8055ce83c3490e4cc0c50
chxy75 <160509015+chxy75@users.noreply.github.com>
2024-04-08 12:57:50 +0000

    final

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 # Built-in imports
-def reverse(text):
+import string
+
+def reverse(text:str) -> str:
+  """
+  reverse a given string
+
+  Parameters
+  ----------
+  text:str
+      the string to be reversed
+
+  Returns
+  -------
+  str
+  """
   if len(text) <= 1:
       return text
   else:
       return reverse(text[1:]) + text[0]
 
-def is_palindrome(text):
-  text =  text.replace('!','').replace('?','').replace(' ','')
-  if text[0] == text[-1]:
-    while len(text) >= 2:
-     is_palindrome(text[1:-1])
-    else :
+def is_palindrome(text:str) -> bool:
+  """
+  check if a given string is palindrome
+
+  Parameters
+  ----------
+  text:str
+      the string to be checked
+
+  Returns
+  -------
+  bool
+  """
+  translator = str.maketrans("", "", string.punctuation)
+  text = text.translate(translator).lower().replace(' ','')
+  
+  if len(text) <= 1:
       return True
   else:
-    return False
+      if text[0] != text[-1]:
+          return False
+      else:
+          return is_palindrome(text[1:-1])
+
+#reverse('hello')
+
+#print(is_palindrome("A man, a plan, a canal: Panama")) 
+  
